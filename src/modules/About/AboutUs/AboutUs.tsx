553157803b2ced8afb9600422f204e3e656e2e96
Andrii Shtohryn <andriishtohryn@gmail.com>
2025-02-09 17:45:02 +0200

    fix build error

--- src/modules/About/AboutUs/AboutUs.tsx
+++ src/modules/About/AboutUs/AboutUs.tsx
@@ -1,6 +1,6 @@
 import ImageGallery from "@/shared/components/ImageGallery/ImageGallery";
 import InfoBlock from "@/shared/components/InfoBlock/InfoBlock";
-import { IAboutUsTranslation } from "@/shared/types";
+import { IInformationBlockTranslation } from "@/shared/types";
 import Link from "next/link";
 
 const aboutImages = [
@@ -17,7 +17,11 @@ const aboutImages = [
     alt: "Team members with pets",
   },
 ];
-const AboutUs = ({ translation }: { translation: IAboutUsTranslation }) => {
+const AboutUs = ({
+  translation,
+}: {
+  translation: IInformationBlockTranslation;
+}) => {
   return (
     <div className="flex flex-col xl:flex-row xl:justify-center xl:items-center xl:bg-white gap-6 xl:gap-0 items-start xl:mb-[40px]">
       <div className="w-full xl:w-1/2 order-2 xl:order-1">
@@ -34,15 +38,15 @@ const AboutUs = ({ translation }: { translation: IAboutUsTranslation }) => {
         <div className="flex flex-col gap-4 xl:flex-row">
           <Link
             className="uppercase px-6 py-3 bg-primary-green text-[18px] leading-[130%] text-[#F8F7F7] text-center rounded-lg transition-colors duration-300 hover:bg-primary-green/90"
-            href={translation.links[0].href}
+            href={translation.links![0].href}
           >
-            {translation.links[0].text}
+            {translation.links![0].text}
           </Link>
           <Link
             className="uppercase px-6 py-3 bg-white text-[18px] leading-[130%] text-primary-green text-center rounded-lg border border-primary-green transition-colors duration-300 hover:bg-primary-green hover:text-white"
-            href={translation.links[1].href}
+            href={translation.links![1].href}
           >
-            {translation.links[1].text}
+            {translation.links![1].text}
           </Link>
         </div>
       </InfoBlock>

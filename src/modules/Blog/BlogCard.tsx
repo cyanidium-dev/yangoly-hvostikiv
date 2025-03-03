6264c51b3161b707da672c6885f9a74397337fed
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-03 19:20:13 +0200

    added blog card styles

--- src/modules/Blog/BlogCard.tsx
+++ src/modules/Blog/BlogCard.tsx
@@ -1,24 +1,40 @@
+import Button from "@/shared/components/Button/Button";
 import { IBlogCardProps } from "@/shared/types";
 import Image from "next/image";
 import React from "react";
+import Link from "next/link";
 
 export default function BlogCard({ blogItem }: IBlogCardProps) {
   const { mainPhoto, date, title, description } = blogItem;
 
   return (
     <li className="max-w-[343px] h-[560px] pt-8 pb-5 px-6 bg-[#FCFCFC] rounded-[20px] shadow-blogCard">
-      <Image
-        src={mainPhoto}
-        alt={title}
-        width={756}
-        height={1008}
-        className="w-full h-[246px] mb-[26px] object-cover object-center rounded-[11.25px]"
-      />
+      <Link href="">
+        <Image
+          src={mainPhoto}
+          alt={title}
+          width={756}
+          height={1008}
+          className="w-full h-[246px] mb-[26px] object-cover object-center rounded-[11.25px]"
+        />
+      </Link>
       <p className="mb-3 text-[16px] font-medium leading-[20.8px] text-black">
         {date}
       </p>
-      <h2 className="mb-3 text-[20px] font-semibold leading-[26px]">{title}</h2>
-      <p className="">{description}</p>
+      <Link href="">
+        <h2
+          className="mb-3 text-[20px] font-semibold leading-[26px] line-clamp-2 focus-visible:text-primary-gray xl:hover:text-primary-gray 
+        transition duration-300 ease-out"
+        >
+          {title}
+        </h2>
+      </Link>
+      <p className="mb-5 font-normal text-[14px] leading-[18.2px] line-clamp-4">
+        {description}
+      </p>
+      <Link href="">
+        <Button text="Детальніше" fullWidth />
+      </Link>
     </li>
   );
 }

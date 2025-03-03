9c9b6332d8c4c4b21871bd7cf57f82c2e0921b68
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-03 22:30:49 +0200

    updated contacts text styles and image

--- src/modules/Blog/Blog.tsx
+++ src/modules/Blog/Blog.tsx
@@ -1,6 +1,7 @@
 import { IBlogProps } from "@/shared/types";
 import React from "react";
 import BlogList from "./BlogList";
+import Contacts from "../Contacts/Contacts";
 
 export default async function Blog({ translation, lang }: IBlogProps) {
   const { title } = translation;
@@ -8,12 +9,13 @@ export default async function Blog({ translation, lang }: IBlogProps) {
   return (
     <section className="mt-[65px] mx-auto xl:mt-[140px] container pt-[60px] xl:pt-8 pb-[100px] xl:pb-[148px] px-4 xl:px-10">
       <h1
-        className="mb-8 xl:mb-12 font-arial font-extrabold text-center text-[24px] xl:text-[32px] leading-[31.2px] 
+        className="mb-8 xl:mb-12 font-arial font-black text-center text-[24px] xl:text-[32px] leading-[31.2px] 
       xl:leading-[41.6px] text-black uppercase"
       >
         {title}
       </h1>
       <BlogList lang={lang} />
+      {/* <Contacts lang={lang} /> */}
     </section>
   );
 }
--- src/modules/Contacts/Contacts.tsx
+++ src/modules/Contacts/Contacts.tsx
@@ -23,47 +23,49 @@ const Contacts = ({ lang, translation }: IContactsProps) => {
       <ContactsMaskDesktop className="absolute inset-0 w-full h-[101%] hidden lg:block" />
       <div className="container relative z-10 px-4 top-[80px] lg:top-[100px] mx-auto ">
         <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
-          <div className="flex flex-col gap-6 lg:max-w-[406px]">
-            <h2 className="text-[24px] text-white leading-[130%] uppercase">
+          <div className="flex flex-col items-center lg:items-start lg:max-w-[406px]">
+            <h2 className="mb-3 font-arial font-black text-[24px] lg:text-[32px] text-white leading-[130%] uppercase text-center lg:text-left">
               {title}
             </h2>
-            <p className="text-[18px] text-white leading-[130%]">{subtitle}</p>
+            <p className="mb-10 lg:mb-12 text-[16px] lg:text-[20px] text-white leading-[130%] text-center lg:text-left">
+              {subtitle}
+            </p>
             <div className="flex flex-col gap-4">
               <a
                 href="tel:+380992004080"
-                className="flex items-center gap-2 text-white leading-[130%] text-[24px] "
+                className="flex items-center gap-2 text-white leading-[130%] text-[20px] lg:text-[24px] font-semibold"
               >
                 <PhoneIcon className="w-6 h-6" /> 38 093 000 00 00
               </a>
               <a
                 href="tel:+380992004080"
-                className="flex items-center gap-2 text-white leading-[130%] text-[24px] "
+                className="flex items-center gap-2 text-white leading-[130%] text-[20px] lg:text-[24px] font-semibold"
               >
                 <PhoneIcon className="w-6 h-6" /> 38 093 000 00 00
               </a>
               <a
                 href="mailto:email@gmail.com"
-                className="flex items-center gap-2 text-white leading-[130%] text-[24px]"
+                className="flex items-center gap-2 text-white leading-[130%] text-[20px] lg:text-[24px] font-semibold"
               >
                 <EmailIcon className="w-6 h-6" variant="secondary" />
                 hvostiki@gmail.com
               </a>
             </div>
           </div>
-          <div className="hidden xl:block absolute bottom-0 right-[42%] z-10">
-            <Image
-              src="/images/cartoon-cat.png"
-              alt="Cartoon Cat"
-              width={411}
-              height={376}
-              priority
-            />
-          </div>
+
           <div className="mt-5 max-w-[533px]  w-full lg:w-[533px] lg:mt-0">
             <UniversalForm onSubmit={handleSubmit} {...contactConfig} />
           </div>
         </div>
       </div>
+      <Image
+        src="/images/two-dogs.webp"
+        alt="Two dogs"
+        width={682}
+        height={974}
+        priority
+        className="hidden xl:block absolute bottom-0 right-[42%] z-10 w-[341px] h-auto"
+      />
     </section>
   );
 };

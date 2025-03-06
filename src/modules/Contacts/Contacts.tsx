aad482365a680ec70416fb917b7f38e24860c80e
Olga Mykhailova <100313107+OlgaMykhailova@users.noreply.github.com>
2025-03-06 11:38:32 +0200

    updated contacts section with new colors

--- src/modules/Contacts/Contacts.tsx
+++ src/modules/Contacts/Contacts.tsx
@@ -2,12 +2,7 @@
 import UniversalForm from "@/shared/components/UniversalForm/UniversalForm";
 import getContactFormConfig from "@/shared/formsConfigs/contactForm";
 import { IContactsProps } from "@/shared/types";
-import {
-  ContactsMask,
-  ContactsMaskDesktop,
-  EmailIcon,
-  PhoneIcon,
-} from "../../../public/images/icons";
+import { EmailIcon, PhoneIcon } from "../../../public/images/icons";
 import Image from "next/image";
 
 const Contacts = ({ lang, translation }: IContactsProps) => {
@@ -18,10 +13,11 @@ const Contacts = ({ lang, translation }: IContactsProps) => {
   };
 
   return (
-    <section id="contacts" className="relative h-[1047px] lg:h-[634px]">
-      <ContactsMask className="absolute inset-0 w-full h-[101%] lg:hidden" />
-      <ContactsMaskDesktop className="absolute inset-0 w-full h-[101%] hidden lg:block" />
-      <div className="container relative z-10 px-4 xl:px-10 top-[245px] lg:top-[113px] mx-auto ">
+    <section
+      id="contacts"
+      className="relative h-[923px] lg:h-[634px] bg-primary-green"
+    >
+      <div className="container relative z-10 px-4 xl:px-10 top-[124px] lg:top-[113px] mx-auto ">
         <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
           <div className="flex flex-col items-center lg:items-start lg:max-w-[406px]">
             <h2 className="mb-3 font-arial font-black text-[24px] lg:text-[32px] text-white leading-[130%] uppercase text-center lg:text-left">
